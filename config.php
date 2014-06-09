9877d945d7fa0ccddd2c5bf28822e16fa1861cd0
Marcos Colombelli Basgal <marcos.colombelli@gmail.com>
2014-06-09 16:27:45 -0300

    primeiro commit

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php 
+//diretório do sistema
+define("BASEPATH", dirname(__FILE__)."/");
+define("BASEURL", "http://localhost/aulas/php/paineladm/");
+define("ADMURL", BASEURL."painel.php");
+define("CLASSESPATH", "classes/");
+define("MODULOSPATH","modulos/");
+define("CSSPATH", "css/");
+define("JSPATH", "js/");
+
+//banco de dados
+define("DBHOST", "localhost");
+define("DBUSER", "root");
+define("DBPASS", "");
+define("DBNAME", "aulas");
+?>
---
+++ footer.php
@@ -0,0 +1,10 @@
+<?php require_once("funcoes.php");
+protegeArquivo(basename(__FILE__));
+?>
+            </div><!-- wrap-content -->
+                <div id="footer">
+                    <p>Painel de Administração | Desenvolvido por RBTech.info</p>
+                </div><!-- footer -->
+        </div><!-- wrapper -->
+    </body>
+</html>
